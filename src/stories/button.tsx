5bf93035d4e034cd369a754fa235be51b0e6371e
Vinh Le <vinh@vertics.co>
2020-06-16 07:57:26 +0300

    feat: configure and add button storybook

---
+++ src/stories/button.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const normalButton = () => <button>Normal</button>
+
+export const disabledButton = () => <button disabled>Disabled</button>
+
+export default {title: 'Button'}
