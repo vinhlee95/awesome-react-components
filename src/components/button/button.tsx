636dee0759004ca89560c02c91eae5dc7c6ce7bf
Vinh Le <vinh@vertics.co>
2020-06-22 08:11:11 +0300

    refactor: correct ButtonProps definition syntax

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react'
 
 export interface BaseButtonProps {
-	children?: React.ReactNode;
+	children?: React.ReactNode
 }
 
 export type ButtonHTMLType = 'submit' | 'button' | 'reset'
 
 export type NativeButtonProps = {
-	onClick?: React.MouseEventHandler<HTMLElement>;
-	htmlType?: ButtonHTMLType;
+	onClick?: React.MouseEventHandler<HTMLElement>
+	htmlType?: ButtonHTMLType
 } & BaseButtonProps &
-	Omit<React.ButtonHTMLAttributes<any>, 'type' | 'onClick'>;
+	Omit<React.ButtonHTMLAttributes<any>, 'type' | 'onClick'>
 
 export type ButtonProps = NativeButtonProps
 
-const Button: React.FunctionComponent = (props: ButtonProps) => {
-
+const Button: React.FunctionComponent<ButtonProps> = props => {
 	const {children, htmlType, ...rest} = props
 
-	const handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => {
-		const {onClick} = props;
+	const handleClick = (
+		e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>,
+	) => {
+		const {onClick} = props
 
 		if (onClick) {
-			(onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>)(e);
+			;(onClick as React.MouseEventHandler<
+				HTMLButtonElement | HTMLAnchorElement
+			>)(e)
 		}
 	}
 
 	return (
-		<button
-			type={htmlType}
-			onClick={handleClick}
-			{...rest}>{children}</button>
+		<button type={htmlType} onClick={handleClick} {...rest}>
+			{children}
+		</button>
 	)
-
 }
 
 export default Button
--- src/components/button/stories/index.stories.tsx
+++ src/components/button/stories/index.stories.tsx
@@ -3,10 +3,14 @@ import Button from '../button'
 
 const handleClicked = () => console.log('Button Clicked')
 
-export const defaultButton = () => <Button onClick={handleClicked}>Default Button</Button>
+export const defaultButton = () => (
+	<Button onClick={handleClicked}>Default Button</Button>
+)
 
 export const disabledButton = () => <Button disabled>Disabled Button</Button>
 
-export const htmlTypeButton = () => <Button htmlType='reset'>Reset Button</Button>
+export const htmlTypeButton = () => (
+	<Button htmlType="reset">Reset Button</Button>
+)
 
 export default {title: 'Button'}
